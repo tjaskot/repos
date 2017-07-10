ffac71ceffd8f7cc9f65a964f2bb67d7968c39cb
Jalisa Greene <jgreene117@yahoo.com>
2017-07-10 10:40:09 -0400

    Welcome Page 7/10

--- save-and-open.js
+++ save-and-open.js
@@ -1,18 +1,19 @@
 function saveTextAsFile()
 {
-    
-    var textToSave = 'extractedvals';
+    var textToSave = document.getElementById("inputTextToSave").value;
     var textToSaveAsBlob = new Blob([textToSave], {type:"text/plain"});
     var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
     var fileNameToSaveAs = document.getElementById("inputFileNameToSaveAs").value;
     var downloadLink = document.createElement("a");
+
     downloadLink.download = fileNameToSaveAs;
     downloadLink.innerHTML = "Download File";
+  
     downloadLink.href = textToSaveAsURL;
     downloadLink.onclick = destroyClickedElement;
+  
     downloadLink.style.display = "none";
     document.body.appendChild(downloadLink);
- 
     downloadLink.click();
 }
 
@@ -23,17 +24,23 @@ function destroyClickedElement(event)
  
 function loadFileAsText()
 {
-    var fileToLoad = document.getElementById("fileToLoad").files[0];
-    var fileReader = new FileReader();
-    fileReader.onload = function(fileLoadedEvent) 
-    {
-        var textFromFileLoaded = fileLoadedEvent.target.result;
-        document.getElementById("inputTextToSave").value = textFromFileLoaded;
-    };
-    fileReader.readAsText(fileToLoad, "UTF-8");
+    var fileName = document.getElementById("fileToLoad").files[0];
+    fileReader = new FileReader();
+    var fileContents;
+    fileReader.onload = function () {
+	fileContents = fileReader.result;
+	console.log(fileContents); 
+    }
+    fileReader.onloadend = function(evt) {
+	localStorage.setItem("file", fileContents);
+	window.open("editor.html", "_self");
+    }
+    fileReader.readAsText(fileName, "utf-8");
 }
 
 function extractedvals() {
     var tobeextractedvals = document.getElementsByClassName('ace_layer ace_text-layer');
-    for (var i=0;i<tobeextractedvals.length;i++){console.log(tobeextractedvals[i].textcontent);}
+    for (var i=0;i<tobeextractedvals.length;i++){
+        console.log(tobeextractedvals[i].textcontent);
+    }
 }
